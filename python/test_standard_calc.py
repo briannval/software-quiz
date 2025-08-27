d04c08ca3b6092368dadb735ada8cd0b49c81886
brianadhitya <brianvalentinoadhitya@gmail.com>
2025-08-26 21:16:36 -0700

    Add more tests

--- python/test_standard_calc.py
+++ python/test_standard_calc.py
@@ -13,6 +13,15 @@ def test_bound_basic1():
     """
 
     assert bound_to_180(0) == 0
+    assert bound_to_180(0.0) == 0.0
+
+    assert bound_to_180(180) == -180
+    assert bound_to_180(30) == 30
+    assert bound_to_180(270) == -90
+    assert bound_to_180(8815) == 175
+
+    assert bound_to_180(179.5) == 179.5
+    assert bound_to_180(179.99999) == 179.99999
 
 
 """ Tests for is_angle_between() """
@@ -20,3 +29,12 @@ def test_bound_basic1():
 
 def test_between_basic1():
     assert is_angle_between(0, 1, 2)
+
+    assert is_angle_between(1, 1, 2)
+    assert is_angle_between(1, 2, 2)
+
+    assert not is_angle_between(45, 90, 270)
+    assert not is_angle_between(10, 50, 350)
+    assert not is_angle_between(10, 50, -10)
+
+    assert is_angle_between(10, 0, 350)
